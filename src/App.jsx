1a284b5898a6b86686e7dac86a8b0cbaa2586cef
elvis <kariuki.elvis@student.moringaschool.com>
2025-04-15 13:09:10 +0300

    add new task

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 import { useState } from 'react';
-import './App.css'
+import './App.css';
 import Button from './components/Button';
-import Todos from './components/Todos';7
+import Todos from './components/Todos';
 import { TODOS } from './data/data';
 
 function App() {
-
   const [todos, setTodos] = useState(TODOS);
+  const [newTodo, setNewTodo] = useState('');
 
-  console.log(todos);
-  
-  
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
-    
-  }
 
+    if (newTodo.trim() === '') return;
+
+    const newTodoItem = {
+      id: Date.now(),
+      text: newTodo
+    };
+
+    setTodos(prevTodos => [...prevTodos, newTodoItem]);
+    setNewTodo('');
+  };
 
   return (
-   <div>
+    <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="todo" />
+        <input 
+          type="text" 
+          name="todo" 
+          value={newTodo} 
+          onChange={(e) => setNewTodo(e.target.value)} 
+        />
         <button type='submit'>Add Todo</button>
       </form>
 
@@ -30,9 +39,8 @@ function App() {
       <Button />
       <Button greeting="jambo" />
       <Button greeting="konichiwa" />
-
-   </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
